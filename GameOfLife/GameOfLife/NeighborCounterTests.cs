a5afbf749e465a72742fe89a0a0897d8b26cde70
Clint Hoagland <c.hoagland@techsmith.com>
2015-10-17 14:34:11 -0400

    concrete NeighborCounter with 1 failing test

---
+++ GameOfLife/GameOfLife/NeighborCounterTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimulatesConway.GameBoardIterator;
+using SimulatesConway.ValueTypes;
+
+namespace GameOfLife
+{
+   [TestClass]
+   public class NeighborCounterTests
+   {
+      private static GameBoardCell AliveCell()
+      {
+         return new GameBoardCell
+         {
+            IsAlive = true
+         };
+      }
+
+      private static GameBoardCell DeadCell()
+      {
+         return new GameBoardCell
+         {
+            IsAlive = false
+         };
+      }
+
+      [TestMethod]
+      public void Count_TwoByTwoWithTwoAliveNeighbors_ReturnsTwo()
+      {
+         GameBoardCell[,] cells =
+         {
+            {
+               DeadCell(), AliveCell()
+            },
+            {
+               DeadCell(), AliveCell()
+            }
+         };
+         var coordinates = new CellCoordinates
+         {
+            X = 0, Y = 0
+         };
+
+         var subject = new NeighborCounter();
+         int count = subject.Count( cells, coordinates );
+
+         count.Should().Be( 2 );
+      }
+   }
+}
---
+++ GameOfLife/SimulatesConway/GameBoardIterator/NeighborCounter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimulatesConway.ValueTypes;
+
+namespace SimulatesConway.GameBoardIterator
+{
+   public class NeighborCounter : INeighborCounter
+   {
+      public int Count( GameBoardCell[,] gameBoardCells, CellCoordinates coordinates )
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
