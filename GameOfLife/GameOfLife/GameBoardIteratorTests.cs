458feabf2c20f7786dcf0b35c3f4dedfd6d37743
Clint Hoagland <c.hoagland@techsmith.com>
2015-10-17 13:48:49 -0400

    iterator interactions now passing

--- GameOfLife/GameOfLife/GameBoardIteratorTests.cs
+++ GameOfLife/GameOfLife/GameBoardIteratorTests.cs
@@ -9,40 +9,47 @@ namespace GameOfLife
    public class GameBoardIteratorTests
    {
       [TestMethod]
-      public void Iterate_CountsNeighbors()
+      public void Iterate_FindsCellCoordinates()
       {
+         var mockCellCoordinateFinder = new Mock<ICellCoordinatesFinder>();
          var mockNeighborCounter = new Mock<INeighborCounter>();
+         var mockCellifeSetter = new Mock<ICellLifeSetter>();
          var gameBoard = new GameBoard( 3, 5 );
 
-         var subject = new GameBoardIterator();
+         var subject = new GameBoardIterator( mockCellCoordinateFinder.Object, mockNeighborCounter.Object, mockCellifeSetter.Object );
          subject.Iterate( gameBoard );
 
-         mockNeighborCounter.Verify( nc => nc.Count( It.IsAny<GameBoardCell[,]>(), It.IsAny<CellCoordinates>()), Times.Exactly(15) );
+         mockCellCoordinateFinder.Verify( ccf => ccf.Find( gameBoard.GameBoardCells, It.IsAny<GameBoardCell>() ), Times.Exactly( 15 ) );
+
+      }
+
+      [TestMethod]
+      public void Iterate_CountsNeighbors()
+      {
+         var mockCellCoordinateFinder = new Mock<ICellCoordinatesFinder>();
+         var mockNeighborCounter = new Mock<INeighborCounter>();
+         var mockCellifeSetter = new Mock<ICellLifeSetter>();
+         var gameBoard = new GameBoard( 3, 5 );
+
+         var subject = new GameBoardIterator( mockCellCoordinateFinder.Object, mockNeighborCounter.Object, mockCellifeSetter.Object );
+         subject.Iterate( gameBoard );
+
+         mockNeighborCounter.Verify( nc => nc.Count( It.IsAny<GameBoardCell[,]>(), It.IsAny<CellCoordinates>() ), Times.Exactly( 15 ) );
       }
 
       [TestMethod]
       public void Iterate_SetsCellsLives()
       {
+         var mockCellCoordinateFinder = new Mock<ICellCoordinatesFinder>();
+         var mockNeighborCounter = new Mock<INeighborCounter>();
          var mockCellLifeSetter = new Mock<ICellLifeSetter>();
          var gameBoard = new GameBoard( 3, 5 );
 
-         var subject = new GameBoardIterator();
+         var subject = new GameBoardIterator( mockCellCoordinateFinder.Object, mockNeighborCounter.Object, mockCellLifeSetter.Object );
          subject.Iterate( gameBoard );
 
          mockCellLifeSetter.Verify( cls => cls.SetLife( It.IsAny<int>(), It.IsAny<GameBoardCell>() ), Times.Exactly( 15 ) );
       }
 
-      [TestMethod]
-      public void Iterate_FindsCellCoordinates()
-      {
-         var mockCellCoordinateFinder = new Mock<ICellCoordinatesFinder>();
-         var gameBoard = new GameBoard( 3, 5 );
-
-         var subject = new GameBoardIterator();
-         subject.Iterate( gameBoard );
-
-         mockCellCoordinateFinder.Verify( ccf => ccf.Find( gameBoard.GameBoardCells, It.IsAny<GameBoardCell>()), Times.Exactly( 15 ) );
-
-      }
    }
 }
--- GameOfLife/SimulatesConway/GameBoardIterator/GameBoardIterator.cs
+++ GameOfLife/SimulatesConway/GameBoardIterator/GameBoardIterator.cs
@@ -9,14 +9,24 @@ namespace SimulatesConway.GameBoardIterator
 {
    public class GameBoardIterator : IGameBoardIterator
    {
-      private INeighborCounter _neighborCounter;
-      private ICellLifeSetter _cellLifeSetter;
+      private readonly ICellCoordinatesFinder _cellCoordinatesFinder;
+      private readonly INeighborCounter _neighborCounter;
+      private readonly ICellLifeSetter _cellLifeSetter;
+
+      public GameBoardIterator( ICellCoordinatesFinder cellCoordinatesFinder, INeighborCounter neighborCounter, ICellLifeSetter cellLifeSetter )
+      {
+         _cellCoordinatesFinder = cellCoordinatesFinder;
+         _neighborCounter = neighborCounter;
+         _cellLifeSetter = cellLifeSetter;
+      }
       public GameBoard Iterate( GameBoard previousGameBoard )
       {
          GameBoardCell[,] gameBoardCells = previousGameBoard.GameBoardCells;
          foreach ( var gameBoardCell in previousGameBoard.GameBoardCells )
          {
-            
+            CellCoordinates coordinates = _cellCoordinatesFinder.Find( gameBoardCells, gameBoardCell );
+            int aliveNeighbors = _neighborCounter.Count( gameBoardCells, coordinates );
+            _cellLifeSetter.SetLife( aliveNeighbors, gameBoardCell );
          }
          return previousGameBoard;
       }
